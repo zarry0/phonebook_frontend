c1dbdb21eea2909506a6fb005e156050fff2dd66
Rodrigo Weller Zarrabal <cs.rgzarrabal@gmail.com>
2024-07-28 20:07:25 -0600

    Moddified - app to produce the production build

--- src/services/communicationUtils.js
+++ src/services/communicationUtils.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001/api/persons';
+const baseURL = '/api/persons';
 
 const getAll = () => {
     return axios.get(baseURL).then(response => response.data)
